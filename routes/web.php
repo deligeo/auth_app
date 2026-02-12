db138f4af1ec958565090827f7faf0373f8c632d
deligeo <georgedelna10@gmail.com>
2026-02-11 19:53:21 -0500

    added Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn()=> redirect('/login'));
+
+Route::get('/register', [AuthController::class,'showRegister']);
+Route::post('/register', [AuthController::class,'register']);
+
+Route::get('/login', [AuthController::class,'showLogin'])->name('login');
+Route::post('/login', [AuthController::class,'login']);
+
+Route::get('/dashboard', [AuthController::class,'dashboard'])->middleware('auth');
+
+Route::post('/logout', [AuthController::class,'logout']);
